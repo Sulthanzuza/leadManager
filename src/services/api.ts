cb4e8fe94e147d5f24d9a8266e43d7757ce3020e
Sulthanzuza <sulthanpriv@gmail.com>
2025-05-11 14:57:50 +0530

    changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Lead, ApiResponse } from '../types';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://leadmanager-9dfm.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
